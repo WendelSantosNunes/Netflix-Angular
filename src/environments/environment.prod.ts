5ef88d706858b087e58c54a153c1330830326785
wendel dos santos nunes <wendelnunes9999@gmail.com>
2024-08-19 19:22:18 -0300

    :sparkles: feat: update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://image.tmdb.org/t/p/w500',
-  apiKey: 'Chave de acessor da API'
+  apiUrl: `${process.env['API_URL']}`,
+  apiKey: `${process.env['API_KEY']}`
 };
